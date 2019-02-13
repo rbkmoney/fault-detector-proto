a391b8a67dbe5fb4dadc4ed9f1f7d751a306208c
d.baykov <d.baykov@rbkmoney.com>
2019-02-13 19:17:25 +0300

    Fix fault detector proto. Change parameters and interfaces [2]

--- proto/fault_detector.thrift
+++ proto/fault_detector.thrift
@@ -5,14 +5,19 @@ namespace erlang fault_detector
 
 typedef base.ID ServiceId
 typedef base.ID RequestId
+typedef i64     Milliseconds
+typedef double  FailureRate
+typedef i16     TimeoutDelta
 
 /** Ответ сервиса определения ошибок на запрос статистики для сервиса */
 struct ServiceStatistics {
 
     /** ID сервиса */
-    1: required ServiceId service_id
-    /** Процент ошибок для данного сервиса */
-    2: required double failure_rate
+    1: required ServiceId   service_id
+    /** Показатель частоты отказов для данного сервиса от 0 до 1, где 0 - это отсутствие ошибок,
+    * а 1 - исключительно сбойные операции
+    */
+    2: required FailureRate failure_rate
 
 }
 
@@ -40,27 +45,39 @@ struct Error {
 * Предполагается, что временные характеристики будут заданы в мс
 **/
 struct ServiceConfig {
-    /** Время жизни операции в рамках сервиса */
-    1: required i64 operation_lifetime
-    /** Время жизни зависшей операции для сервиса (если не задать, то аналогично обычной операции) */
-    2: optional i64 hovering_operation_lifetime
-    /** Временной интервал для "скользящего окна" - время в рамках которого будут ьраться транзакции */
-    3: optional i64 sliding_window
-    /** Время, после которого операция считается зависшей */
-    4: optional i64 hovering_operation_error_delay
-    /** Значение параметра инициализируюзего параметра откащов сервиса (значение от 0 до 1) */
-    5: optional double init_failure_rate
+    /** Время жизни операции в рамках сервиса (в рамках данного времени будет рассчитано среднее время выполнения операции) */
+    1: required Milliseconds operation_lifetime
+    /** Временной интервал для "скользящего окна" (время в рамках которого будут браться операции для расчета статистики) */
+    2: required Milliseconds sliding_window
+    /**
+    * Таймаут дельта. Использвется при определении операций, которые не уложились в планируемое время выполнения.
+    * Задается как десятичное число как отображение дельты в процентах. Например, если timeout_delta = 10, а среднее
+    * время выполнения 4000мс, то операции превысившие 4400мс будут считаться как сбойные (если не задан
+    * hovering_operation_error_delay больший чем 4400)
+    */
+    3: optional TimeoutDelta timeout_delta
+    /** Время, после которого операция считается зависшей. Если не задано, то будет равно среднему времени
+    * выполнения транзакции + дельта
+    */
+    4: optional Milliseconds hovering_operation_error_delay
+    /** Значение инициализируюзего параметра отказа сервиса (значение от 0 до 1). Если задать, то приведет значение
+     * уровня ошибок до определенного уровня путем вставки операций-пустышек. Может понадобиться для опримизации
+     * "отзывчивости" сервиса на сбой
+     */
+    5: optional FailureRate  init_failure_rate
 }
+
+exception ServiceNotFoundException {}
 
 service FaultDetector {
 
     /** Инициализация параметров сервиса */
     void InitService(1: ServiceId service_id, 2: ServiceConfig service_config)
-    /** Получение статистики по сервису */
-    ServiceStatistics GetStatistics(1: ServiceId service_id)
+    /** Получение статистики по сервисам */
+    list<ServiceStatistics> GetStatistics(1: list<ServiceId> services)
     /** Регистрация операции сервиса **/
-    void RegisterOperation(1: ServiceId service_id, 2: RequestId request_id, 3: Operation operation)
+    void RegisterOperation(1: ServiceId service_id, 2: RequestId request_id, 3: Operation operation) throws (1: ServiceNotFoundException ex1)
     /** Сброс/Установка статистики сервиса **/
-    void UpdateServiceConfig(1: ServiceId service_id, 2: ServiceConfig service_config)
+    void UpdateServiceConfig(1: ServiceId service_id, 2: ServiceConfig service_config) throws (1: ServiceNotFoundException ex1)
 
 }
